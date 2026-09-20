4d70e8f09100fd18072f2472cd383ea7a2a247aa
Osome55 <oscarlearymalone@gmail.com>
2026-09-20 14:54:53 -0400

    Gambler logic implementation

--- Assets/Scripts/DefaultGambler.cs
+++ Assets/Scripts/DefaultGambler.cs
@@ -18,8 +18,33 @@ public class DefaultGambler : GamblerLogic
     //How the gambler's behavior will change upon a major loss
     public override void majorLoss() {
         //Lose everything they spent
-        maxPatience -= 15.0;
-        bet -= 10.0;
+        switch (mood) {
+            case Mood.Ecstatic: {
+                mood = Mood.Neutral;
+                timesTillMoodChange = 3;
+                bet -= 15;
+                patience -= 15;
+                break;
+            } case Mood.Happy: {
+                mood = Mood.Annoyed;
+                timesTillMoodChange = 3;
+                bet -=10;
+                patience -= 10;
+                break;
+            } case Mood.Neutral: {
+                mood = Mood.Angry;
+                timesTillMoodChange = 3;
+                bet += 10;
+                patience -= 20;
+                break;
+            } case Mood.Annoyed: {
+                endGamble();
+                break;
+            } case Mood.Angry: {
+                endGamble();
+                break;
+            }
+        }
         gambleOngoing = false;
         newGamble();
     }
@@ -29,8 +54,39 @@ public class DefaultGambler : GamblerLogic
         //Gain back half the money they spent
         money += lastBet * 0.5;
         updateMoney(lastBet*0.5);
-        maxPatience -= 1.0;
-        bet += 5;
+        switch (mood) {
+            case Mood.Ecstatic: {
+                mood = Mood.Happy;
+                timesTillMoodChange = 3;
+                bet -= 5;
+                patience -= 5;
+                break;
+            } case Mood.Happy: {
+                mood = Mood.Neutral;
+                timesTillMoodChange = 3;
+                bet -= 10;
+                patience -= 5;
+                break;
+            } case Mood.Neutral: {
+                timesTillMoodChange -= 1;
+                if (timesTillMoodChange <= 0) {
+                    mood = Mood.Annoyed;
+                    timesTillMoodChange = 3;
+                    bet -= 5;
+                    patience -= 5;
+                }
+                break;
+            } case Mood.Annoyed: {
+                mood = Mood.Angry;
+                timesTillMoodChange = 3;
+                bet -= 5;
+                patience -= 10;
+                break;
+            } case Mood.Angry: {
+                endGamble();
+                break;
+            }
+        }
         gambleOngoing = false;
         newGamble();
     }
@@ -40,8 +96,46 @@ public class DefaultGambler : GamblerLogic
         //Gain 1.5x what they spent
         money += lastBet * 1.5;
         updateMoney(lastBet*1.5);
-        maxPatience += 5.0;
-        bet += 10.0;
+        switch (mood) {
+            case Mood.Ecstatic: {
+                bet += 5;
+                patience += 1;
+                break;
+            } case Mood.Happy: {
+                timesTillMoodChange -= 1;
+                if (timesTillMoodChange <= 0) {
+                   mood = Mood.Ecstatic;
+                   timesTillMoodChange = 3;
+                   bet += 15;
+                   patience -= 10; 
+                }
+                break;
+            } case Mood.Neutral: {
+                mood = Mood.Happy;
+                timesTillMoodChange = 3;
+                bet += 10;
+                patience += 5;
+                break;
+            } case Mood.Annoyed: {
+                timesTillMoodChange -= 1;
+                if (timesTillMoodChange <= 0) {
+                   mood = Mood.Neutral;
+                   timesTillMoodChange = 3;
+                   bet += 5;
+                   patience += 10; 
+                }
+                break;
+            } case Mood.Angry: {
+                timesTillMoodChange -= 1;
+                if (timesTillMoodChange <= 0) {
+                   mood = Mood.Ecstatic;
+                   timesTillMoodChange = 3;
+                   bet += 5;
+                   patience += 5; 
+                }
+                break;
+            }
+        }
         gambleOngoing = false;
         newGamble();
     }
@@ -51,8 +145,36 @@ public class DefaultGambler : GamblerLogic
         //Gain 3x what they spent
         money += lastBet * 3;
         updateMoney(lastBet*3);
-        maxPatience += 15.0;
-        bet *= 1.5;
+        switch (mood) {
+            case Mood.Ecstatic: {
+                bet += 15;
+                maxPatience -= 10;
+                break;
+            } case Mood.Happy: {
+                mood = Mood.Ecstatic;
+                timesTillMoodChange = 3;
+                bet += 10;
+                maxPatience -= 5;
+                break;
+            } case Mood.Neutral: {
+                mood = Mood.Ecstatic;
+                timesTillMoodChange = 3;
+                bet += 15;
+                maxPatience -= 15;
+                break;
+            } case Mood.Annoyed: {
+                mood = Mood.Neutral;
+                timesTillMoodChange = 3;
+                maxPatience += 10;
+                break;
+            } case Mood.Angry: {
+                mood = Mood.Neutral;
+                timesTillMoodChange = 3;
+                bet -= 5;
+                patience += 5;
+                break;
+            }
+        }
         gambleOngoing = false;
         newGamble();
     }
